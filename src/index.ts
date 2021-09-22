a2d91d9adbaa2788e30855cfa6ba5770dc156db6
Denis Saenko <denissaenkoo@gmail.com>
2021-09-22 20:22:09 +0300

    cosmetic changes + unused api removed

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@ import { ValidationResult } from "./result";
 import { ValidationChain } from "./chain/validation-chain";
 import { Context } from "./context/context";
 
-export type ArrayValidationSchema = [unknown, unknown];
-
 export async function validate(
   objectToValidate: unknown,
   schema: unknown,
   path: string[] = [],
   stopOnFail = false
 ): Promise<ValidationResult> {
-  if (schema === undefined) throw "Validation schema cannot be undefined!";
-  if (schema === null) throw "Validation schema cannot be null!";
+  if (schema === undefined)
+    throw new Error("Validation schema cannot be undefined!");
+  if (schema === null) throw new Error("Validation schema cannot be null!");
   if (Array.isArray(schema))
-    throw "Validation schema cannot be array! Use build().isArray(schema?) to validate arrays";
+    throw new Error(
+      "Validation schema cannot be array! Use build().isArray(schema?) to validate arrays"
+    );
 
   if ((schema as ValidationChain).context instanceof Context) {
     const context: Context = (schema as ValidationChain).context;
